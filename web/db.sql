e2cfee0713b84de68da0543383b58813463c7ef2
dposlt <david.poslt@gmail.com>
2018-06-12 20:24:34 +0200

    login table

--- web/db.sql
+++ web/db.sql
@@ -7,3 +7,20 @@ CREATE TABLE phone_number (
 );
 
 INSERT INTO phone_number (number, date) VALUES ('777333222',NOW());
+
+CREATE TABLE access (
+ ID int NOT NULL UNIQUE AUTO_INCREMENT,
+ name varchar(50) NOT NULL,
+ heslo varchar(100) NOT NULL, 
+ date DATETIME NOT NULL,
+ PRIMARY KEY (ID)
+);
+
+CREATE TABLE login (
+ ID int NOT NULL UNIQUE AUTO_INCREMENT,
+ user varchar(50) NOT NULL, 
+ date DATETIME NOT NULL,
+ PRIMARY KEY (ID)
+);
+
+INSERT INTO access (name,heslo) VALUES ('david','heslo');
